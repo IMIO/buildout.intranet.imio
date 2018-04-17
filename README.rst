330bdb58d7ff0e23c3818db580e89be39e2a2ea8
Benoît Suttor <bsuttor@imio.be>
2018-04-17 12:08:15 +0200

    Update setuptools and zc.buildout

--- README.rst
+++ README.rst
@@ -8,7 +8,7 @@ This repo is used to build imio intranet
 
 Dev
 ---
-To use this in dev, you have to install docker and docker-compose, and after
+To use this in dev, you have to install docker and docker-compose, and after :
 
     make build
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 #     ./bootstrap.sh  # use buildout.cfg
 #     ./bootstrap.sh -c dev.cfg  # use dev.cfg
 ln -s dev.cfg buildout.cfg
-pip install --user -r https://raw.githubusercontent.com/plone/buildout.coredev/5.1/requirements.txt
+pip install --user -r requirements.txt
 buildout "$@"
