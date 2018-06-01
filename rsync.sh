0775aeb30c074cf7fb6b77ef858af9006a31c8bc
Benoit Suttor <bsuttor@imio.be>
2018-06-01 11:42:41 +0200

    Delete other branch files

--- rsync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rsync -P imio@intranet-imio.imio.be:/srv/instances/intranet_imio/filestorage/Data.fs var/filestorage/Data.fs
-echo 'blobstorage'
-rsync -r --info=progress2 imio@intranet-imio.imio.be:/srv/instances/intranet_imio/blobstorage/ var/blobstorage/
