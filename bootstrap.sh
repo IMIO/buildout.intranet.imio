fd0a89a4631c7b893fba21b27cfa56206c8af0b7
Benoit Suttor <bsuttor@imio.be>
2018-06-01 11:29:39 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,6 @@
 #     ./bootstrap.sh  # use buildout.cfg
 #     ./bootstrap.sh -c dev.cfg  # use dev.cfg
 ln -s dev.cfg buildout.cfg
-pip install -I --user -r requirements.txt
-~/.local/bin/buildout "$@"
+virtualenv -p python2.7 .
+bin/pip install -I -r requirements.txt
+bin/buildout "$@"
